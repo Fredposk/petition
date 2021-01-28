d48d2e1c5b82b44f12cd68f340290dba99a50403
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-01-28 12:52:48 +0100

    heroku ico

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const favicon = require('serve-favicon');
-const path = require('path');
 const app = express();
 // For testing purposes
 exports.app = app;
@@ -29,9 +27,6 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 // Logging middleware
 app.use(morgan('dev'));
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 // Security middleware
 app.use(
     helmet({
